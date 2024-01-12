e04e797ba794096759638de7f2c77086fb3830e2
Pedro Bohrer <pedrombohrer@gmail.com>
2024-01-12 00:14:49 -0300

    first

---
+++ js/README.md
@@ -0,0 +1,3 @@
+## JavaScript Exercise of Secret Friend Randomizer
+### Done with the use of arrays and array manipulation, creation of a randomizer and conditional to ensure they dont repeat
+# Orginal code made with <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" width="40" height="40"/>
---
+++ js/app.js
@@ -0,0 +1,48 @@
+let listaAmigo = [];
+let sorteio = document.getElementById("lista-sorteio");
+function adicionar() {
+    let amigo = document.getElementById("nome-amigo");
+    listaAmigo.push(amigo.value);
+    let lista = document.getElementById("lista-amigos");
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
+    amigo.value = "";
+
+}
+function sortear() {
+    //arrays para os dois lados da lista
+    let sorteamento = []
+    let sorteamento2 = []
+    for (y = 0; y < listaAmigo.length;) {
+    //aleatoriedade das listas
+        let aleatorio = parseInt(Math.random() * listaAmigo.length);
+        let aleatorio2 = parseInt(Math.random() * listaAmigo.length);
+    //condicoes para garantir que a lista nao se repita
+        if (sorteamento.includes(aleatorio)){
+            aleatorio = parseInt(Math.random() * listaAmigo.length);
+        } else if (sorteamento2.includes(aleatorio2)) {
+            aleatorio2 = parseInt(Math.random() * listaAmigo.length);
+        } else if (aleatorio == aleatorio2) {
+            sorteamento = [];
+            sorteamento2 = [];
+        }else {
+            sorteamento.push(aleatorio);
+            sorteamento2.push(aleatorio2);
+            y++
+        }
+    }
+    for (m = 0; m < listaAmigo.length; m++){
+    sorteio.innerHTML = sorteio.innerHTML + "<br>" + `${listaAmigo[sorteamento[m]]} -> ${listaAmigo[sorteamento2[m]]}`;
+}
+}
+
+function reiniciar() {
+    document.getElementById("lista-amigos").innerHTML = "";
+    sorteio.innerHTML = ""
+    listaAmigo = [];
+    sorteamento = [];
+    sorteamento2 = [];
+}
